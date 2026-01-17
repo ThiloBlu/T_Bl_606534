7c1a3426ad8f9db4d4ccc18c9c539ac5514759df
Thilo <thilo.blumenberg@live.de>
2026-01-17 15:10:47 +0100

    created new branch and changed math.java

--- src/main/java/tuc/isse/uebung01/exercise3/math.java
+++ src/main/java/tuc/isse/uebung01/exercise3/math.java
@@ -2,7 +2,7 @@ package exercise3;
 
 public class math {
     public static void fcn(int x, int y, int z){
-       float result = 6 - x/2 + x*y + z/x*y; /* float result = 6 - x/2 + x*(y + z/x*y);*/
+       float result = 6 - x/2 + x*(y + z/x*y); /* float result = 6 - x/2 + x*y + z/x*y;*/
        System.out.println("Ergebins: " + result);
     } 
     public static void main(String[] args){
