20722a9136a4f394361a44d751100d2e604ed8d5
Thilo <thilo.blumenberg@live.de>
2026-01-18 19:00:27 +0100

    added the doc of the math.java

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Heart"},{"p":"<Unnamed>","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"exercise3","l":"math"}]
--- src/main/java/tuc/isse/uebung01/exercise3/math.java
+++ src/main/java/tuc/isse/uebung01/exercise3/math.java
@@ -1,7 +1,21 @@
 package exercise3;
 
+/**
+ * Methode zur Ausführung der Berechnung f(x,y,z) = 6 - x/2 + x*(y + z/x*y)
+ * @author Thilo Blumenberg
+ * @version 1.1 
+*/
+
 public class math {
-    
+
+    /**
+     * Definition der Funktion f(x,y,z) = 6 - x/2 + x*(y + z/x*y)
+     * @param x
+     * @param y
+     * @param z
+     * @return
+     */
+
     public static float fcn(int x, int y, int z){
         if(x == 0){
             return 0;
@@ -15,6 +29,13 @@ public class math {
         }
 
     } 
+
+    /**
+     * Main Methode
+     * @param args[0]: Operand 1 
+     * @param args[1]: Operand 2
+     * @param args[2]: Operand 3
+     */
     public static void main(String[] args){
         fcn(Integer.parseInt(args[0]), Integer.parseInt(args[1]),Integer.parseInt(args[2]));
     }
